db3c4b5a4acf996ecbf611b72e43a654fef57d1e
shivam-batham <rsb7214@gmail.com>
2023-12-05 22:46:54 +0530

    user route added

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const ErrorHandler = require("./utils/errorHandler.js");
 //
 app.use(express.json());
 // routes imports
+const user = require("./routes/userRoute.js");
+app.use("/api/v1/",user);
 const product = require("./routes/productRoute");
 app.use("/api/v1/",product);
 // middleware for error
---
+++ backend/routes/userRoute.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = require("router");
+const { registerUser } = require("../controllers/userController");
+
+router.route("/register").post(registerUser)
+
+module.exports = router;
