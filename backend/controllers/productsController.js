3f340d53003b3f7594f8834b8fc715d6b76688df
shivam-batham <rsb7214@gmail.com>
2023-12-08 00:15:29 +0530

    product Schema updated

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -6,6 +6,7 @@ const ApiFeatures = require("../utils/apiFeatures.js");
 
 //create product -- Admin
 exports.createProduct = catchAsyncError(async (req, res, next) => {
+  req.body.user = req.user.id;
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
