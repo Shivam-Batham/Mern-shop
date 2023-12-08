397696c1f36474bd04ad8656faba5f6990a7ce51
shivam-batham <rsb7214@gmail.com>
2023-12-08 22:47:27 +0530

    bakcend authentication completed

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -9,7 +9,21 @@ module.exports = (err,req, res, next)=>{
         const message = `Resource not found, Invalid: ${err.path}`;
         err = new ErrorHandler(message,400);
     }
-
+    //wrong JWT web token err
+    if(err.name === "JsonWebTokenError"){
+        const message = `Json web token is invalid, try again`;
+        err = new ErrorHandler(message,400);
+    }
+    // JWT expire error
+    if(err.name === "TokenExpiredError"){
+        const message = `Json web token is Expired, try again`;
+        err = new ErrorHandler(message,400);
+    }
+    // duplicate mogngoDB error
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keyValue} entered`;
+        err = new ErrorHandler(message,400);
+    }
     console.log(err.statusCodes);
     res.status(err.statusCode).json({
         success : false,
