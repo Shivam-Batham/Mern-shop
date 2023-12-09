030d21f09c636b39068666ac3d83ad74abc5a5d1
shivam-batham <rsb7214@gmail.com>
2023-12-09 20:16:37 +0530

    product review controller added,and some minor changes in Product Schema bcz of ratings

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -14,46 +14,51 @@ const productSchema = new mongoose.Schema({
     required: [true, "please Enter price"],
     maxLength: [8, "price cannot exceed 8 charecters"],
   },
-  rating: {
+  ratings: {
     type: Number,
     default: 0,
   },
   image: [
     {
-      public_id: {type: String, required: true },
-      url :{type: String, required: true },
-    }
+      public_id: { type: String, required: true },
+      url: { type: String, required: true },
+    },
   ],
-  category:{
-    type :String,
-    required : [true,"please Enter Product category"]
+  category: {
+    type: String,
+    required: [true, "please Enter Product category"],
   },
-  Stock : {
-    type:Number,
-    required : [true,"please Enter Product stock"],
+  Stock: {
+    type: Number,
+    required: [true, "please Enter Product stock"],
     maxLength: [4, "price cannot exceed 4 charecters"],
-    default:1
+    default: 1,
   },
-  numOfReviews : {
-    type :Number,
-    default : 0
+  numOfReviews: {
+    type: Number,
+    default: 0,
   },
-  reviews :[
+  reviews: [
     {
-        name:{type :String, required:true},
-        rating : {type:Number, required : true},
-        comment : {type :String,required:true}
-    }
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      name: { type: String, required: true },
+      rating: { type: Number, required: true },
+      comment: { type: String, required: true },
+    },
   ],
-  user:{
-    type : mongoose.Schema.ObjectId,
-    ref : "User",
-    required:true,
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
   },
-  createdAt : {
-    type : Date,
-    default : Date.now
-  }
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Product", productSchema);
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllProducts,createProduct,updateProduct, deleteProduct, getProductDetails } = require("../controllers/productsController");
+const { getAllProducts,createProduct,updateProduct, deleteProduct, getProductDetails, createProductReview } = require("../controllers/productsController");
 const { isAuthenticatedUser,autherizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
@@ -9,6 +9,8 @@ router.route("/product/new").post(isAuthenticatedUser,autherizeRoles("admin"),cr
 router.route("/product/:id").put(isAuthenticatedUser,autherizeRoles("admin"),updateProduct);
 router.route("/product/:id").delete(isAuthenticatedUser,autherizeRoles("admin"),deleteProduct);
 router.route("/product/:id").get(getProductDetails);
+router.route("/review").put(isAuthenticatedUser,createProductReview);
+
 
 
 module.exports = router; 
