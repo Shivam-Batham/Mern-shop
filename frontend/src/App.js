96e9e6aef0efdb9c88fc11be90102ecd69c1dd8e
shivam-batham <rsb7214@gmail.com>
2023-12-11 01:43:55 +0530

    Header Footer created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/layout/Header/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
+import webFont from "webfontloader";
+import React from "react";
+import Footer from "./components/layout/Footer/Footer.jsx";
 
 function App() {
+  React.useEffect(()=>{
+    webFont.load({
+      google:{
+        families:["Roboto","Droid sans","chilanka"]
+      }
+    })
+  },[]);
   return (
-  <>
-    
-  </>    
+    <>
+      <Router>
+        <Header />
+        <Footer />
+      </Router>
+    </>
   );
 }
 
